d6fc49cd6af5b4e2fcfb197708c2c537fe407ca3
Eznone <enzotresmediano@gmail.com>
2024-09-30 17:00:33 -0300

    finished watershed

--- Tarefa 5/countingCoins.py
+++ Tarefa 5/countingCoins.py
@@ -46,12 +46,52 @@ def hough_circle(originalImage):
           cv2.circle(originalImage, center, radius, (255, 0, 255), 3)
   images.append(originalImage)
 
+def watershed(originalImage):
+    gammaImage = adjust_gamma(image, 1.0)
+    grayImage = cv2.cvtColor(gammaImage, cv2.COLOR_BGR2GRAY)
+    grayImage = cv2.GaussianBlur(grayImage, (5, 5), 0)
+    _, thresh = cv2.threshold(grayImage, 250, 255, cv2.THRESH_BINARY_INV)
+
+    # noise removal
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+    
+    # sure background area
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+    
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2,5)
+    _, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(),255,0)
+    
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg,sure_fg)
+
+    # Marker labelling
+    _, markers = cv2.connectedComponents(sure_fg)
+    
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers+1
+    
+    # Now, mark the region of unknown with zero
+    markers[unknown==255] = 0
+
+    markers = cv2.watershed(originalImage,markers)
+    originalImage[markers == -1] = [255,0,0]
+
+    #cv2.normalize(dist_transform, dist_transform, 0, 1.0, cv2.NORM_MINMAX)
+    #cv2.imshow("dist_transform", dist_transform)
+    #cv2.waitKey(0)
+
+    images.append(originalImage)
 
 # Main ----------------------------------------------------------------------
 
 # First method is matching templates as the function suggests
 template_match(image.copy(), coinComp.copy())
 hough_circle(image.copy())
+watershed(image.copy())
+
 # Making grid layout
 presentationImage = auto_image_grid(images)
 cv2.imshow("Hough Image", presentationImage)
